f8a3111652703f16044de1b5cf12e795cb984e68
Naplesoul <shenwhang@outlook.com>
2022-03-21 19:13:47 +0800

    improve: remove lab2 from this branch

--- bind.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo ifconfig ens33 down
-sudo modprobe uio
-cd /home/shen/Projects/DPDK/dpdk/kernel/linux/igb_uio
-make
-sudo insmod igb_uio.ko
-cd ../../..
-sudo usertools/dpdk-devbind.py --bind=igb_uio ens33
-sudo usertools/dpdk-devbind.py -s
